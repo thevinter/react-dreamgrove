cd28934a3f6294ddf33586982846945ca4266b71
thevinter <contact@thevinter.com>
2024-06-30 07:41:08 +0200

    fix: changing how wowhead script loads to allow hot reload

--- app/layout.tsx
+++ app/layout.tsx
@@ -76,7 +76,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       <body className="bg-white pl-[calc(100vw-100%)] text-black antialiased dark:bg-[#282828] dark:text-white">
-        <Script src="https://wow.zamimg.com/widgets/power.js" strategy="afterInteractive" />
         <ThemeProviders>
           <Analytics analyticsConfig={siteMetadata.analytics as AnalyticsConfig} />
           <SectionContainer>
